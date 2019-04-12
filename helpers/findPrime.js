f0e7540bc9e05907611d6151b422214e47b35215
Pedro Neto <pedro.diogo.neto@outlook.pt>
2019-04-12 13:15:30 +0200

    Refactor findPrime method

--- helpers/findPrime.js
+++ helpers/findPrime.js
@@ -8,82 +8,45 @@ module.exports = function findPrime(numberToFind) {
     const isSecondEqualThird = numberToFindArray[1] === numberToFindArray[2]
     const isFirstEqualThird = numberToFindArray[0] === numberToFindArray[2]
 
+    numIndexExistsInString = (stringToCheck, index) => stringToCheck.includes(numberToFindArray[index])
+    
+    indexInString = (stringToCheck, index) => stringToCheck.indexOf(numberToFindArray[index])
+
+    stringWithoutRepeatedLetter = (stringToSplice, index) => {
+        const indexToSplice = stringToSplice.indexOf(numberToFindArray[index])
+        let copyStringToSplice = JSON.parse(JSON.stringify(stringToSplice))
+        copyStringToSplice.splice(indexToSplice, 1)
+        return copyStringToSplice
+    }
+
+    conditionToFindPrimeInString = (stringToCheck1, stringToCheck2, indexCompensationFromSplice1, indexCompensationFromSplice2, numberString ) => {
+        if(numIndexExistsInString(stringToCheck1, 1) && numIndexExistsInString(stringToCheck2, 2)
+            && (indexInString(stringToCheck1, 1) + indexCompensationFromSplice1) > (indexInString(numberString, 0))
+            && (indexInString(stringToCheck2, 2) + indexCompensationFromSplice2) > (indexInString(stringToCheck1, 1) + indexCompensationFromSplice1)) {                
+                return numberString.join('') 
+            }
+    }
+
     return list.find(number => {
 
         if( number.toString().length < 3 ) return
 
         const numberString = number.toString().split('')
+        const stringWithoutFirst = stringWithoutRepeatedLetter(numberString, 0)
+        const stringWithoutSecond = stringWithoutRepeatedLetter(numberString, 1)
+        const stringWithoutFirstAndSecond = stringWithoutRepeatedLetter(stringWithoutFirst, 1)
 
-        if(numberString.includes(numberToFindArray[0])) {
-
-            if(isFirstEqualSecond && isSecondEqualThird) {
-
-                const indexFirst = numberString.indexOf(numberToFindArray[0])
-                let stringWithoutFirst = JSON.parse(JSON.stringify(numberString))
-                stringWithoutFirst.splice(indexFirst, 1)
-
-                if(numberString.includes(numberToFindArray[1]) 
-                    && (stringWithoutFirst.indexOf(numberToFindArray[1]) + 1) > numberString.indexOf(numberToFindArray[0])) {
-                    
-                    const indexSecond = numberString.indexOf(numberToFindArray[1])
-                    let stringWithoutSecond = JSON.parse(JSON.stringify(stringWithoutFirst)) 
-                    stringWithoutSecond.splice(indexSecond, 1)
-                    
-
-                    if(numberString.includes(numberToFindArray[2]) 
-                        && (stringWithoutSecond.indexOf(numberToFindArray[2]) + 2) > (stringWithoutFirst.indexOf(numberToFindArray[1])+1)) {
-                        return numberString.join('')
-                    }
-                }
-            }
+        if(numIndexExistsInString(numberString, 0)) {
+            
+            if(isFirstEqualSecond && isSecondEqualThird) return conditionToFindPrimeInString(stringWithoutFirst, stringWithoutFirstAndSecond, 1, 2, numberString)
 
             if(isFirstEqualSecond || isSecondEqualThird || isFirstEqualThird) {
 
-                if(isFirstEqualSecond) {
-
-                    const indexFirst = numberString.indexOf(numberToFindArray[0])
-                    let stringWithoutFirst = JSON.parse(JSON.stringify(numberString))
-                    stringWithoutFirst.splice(indexFirst, 1)
-    
-                    if(stringWithoutFirst.includes(numberToFindArray[1]) && (stringWithoutFirst.indexOf(numberToFindArray[1]) + 1) > numberString.indexOf(numberToFindArray[0])) {
-                        
-                        const indexFirst = numberString.indexOf(numberToFindArray[0])
-                        let stringWithoutFirst = JSON.parse(JSON.stringify(numberString)) 
-                        stringWithoutFirst.splice(indexFirst, 1)
-                        
-                        if(numberString.includes(numberToFindArray[2]) && (stringWithoutFirst.indexOf(numberToFindArray[2]) + 1) > stringWithoutFirst.indexOf(numberToFindArray[1])+1) {
-                            return numberString.join('')
-                        }          
-                    }
-                } else if (isFirstEqualThird) {
-                    if(numberString.includes(numberToFindArray[1]) && numberString.indexOf(numberToFindArray[1]) > numberString.indexOf(numberToFindArray[0])) {
-
-                        const indexFirst = numberString.indexOf(numberToFindArray[0])
-                        let stringWithoutFirst = JSON.parse(JSON.stringify(numberString)) 
-                        stringWithoutFirst.splice(indexFirst, 1)
-
-                        if(numberString.includes(numberToFindArray[2]) && (stringWithoutFirst.indexOf(numberToFindArray[2]) + 1) > numberString.indexOf(numberToFindArray[1])) {
-                            return numberString.join('')
-                        }
-                    }
-                } else {
-                    if(numberString.includes(numberToFindArray[1]) && numberString.indexOf(numberToFindArray[1]) > numberString.indexOf(numberToFindArray[0])) {
-                        const indexSecond = numberString.indexOf(numberToFindArray[1])
-                        let stringWithoutSecond = JSON.parse(JSON.stringify(numberString)) 
-                        stringWithoutSecond.splice(indexSecond, 1)
-
-                        if(numberString.includes(numberToFindArray[2]) && (stringWithoutSecond.indexOf(numberToFindArray[2]) + 1) > numberString.indexOf(numberToFindArray[1])) {
-                            return numberString.join('')
-                        }
-                    }
-                }
-            } else {
-                if(numberString.includes(numberToFindArray[1]) && numberString.indexOf(numberToFindArray[1]) > numberString.indexOf(numberToFindArray[0])) {
-                    if(numberString.includes(numberToFindArray[2]) && numberString.indexOf(numberToFindArray[2]) > numberString.indexOf(numberToFindArray[1])) {
-                        return numberString.join('')
-                    }
-                }
-            }
+                if(isFirstEqualSecond) return conditionToFindPrimeInString(stringWithoutFirst, stringWithoutFirst, 1, 1, numberString)
+                else if (isFirstEqualThird) return conditionToFindPrimeInString(stringWithoutFirst, stringWithoutFirst, 0, 1, numberString)
+                else return conditionToFindPrimeInString(numberString, stringWithoutSecond, 0, 1, numberString)
+            
+            } else return conditionToFindPrimeInString(numberString, numberString, 0, 0, numberString)
         }
     })
 }
